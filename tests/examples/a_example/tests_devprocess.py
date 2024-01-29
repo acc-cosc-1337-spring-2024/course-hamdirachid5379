4179ddddd6422253db3fcd88b0469a3466aa17b2
hamdirachid5379 <157082615+hamdirachid5379@users.noreply.github.com>
2024-01-29 21:30:17 +0000

    dev process, code and test, use in main

--- tests/examples/a_example/tests_devprocess.py
+++ tests/examples/a_example/tests_devprocess.py
@@ -4,6 +4,6 @@ from src.examples.a_example.devprocess import add_numbers
 
 class Test_Config(unittest.TestCase):
 
-    def test_add_numbers_2(self):
-        self.assertEqual(2, add_numbers(1, 1))
+    def test_add_numbers_2(self):#create one test function that add_numbers works as expected
+        self.assertEqual(2, add_numbers(1, 2))
 
