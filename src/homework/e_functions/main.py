d619490f40116d0e039480ca3efdbd9a13d3dc0d
hamdirachid5379 <157082615+hamdirachid5379@users.noreply.github.com>
2024-03-04 06:01:35 +0000

    homework4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,5 @@
-#
+#use the functions
+
+
+
+
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,23 @@
-#
+#create functions 
+def get_hour(epoch_seconds):# get the hour since 1970
+    return (epoch_seconds // 3600 ) % 60 % 24
+
+def get_minutes(epoch_seconds): # get the minutes since 1970
+    return (epoch_seconds // 60) % 60
+
+def get_seconds(epoch_seconds):# get the seconds since 1970
+    return (epoch_seconds % 3600) % 60
+
+s=int(input("Enter epoch_seconds:"))
+#1hr = 3600s
+hour = s//3600 % 60 %24
+second = s%60
+minutes = s//60 % 60
+print("%d:%d:%d" %(hour,minutes,second))
+
+
+
+
+
+
+
