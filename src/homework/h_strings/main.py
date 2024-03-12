c7d614cc03e87dfe2b0e82c0c152aebeb39a71c1
hamdirachid5379 <157082615+hamdirachid5379@users.noreply.github.com>
2024-03-12 04:07:41 +0000

    homework5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,29 @@
-#
+#Create a Menu
+
+from homework.h_strings.strings import get_hamming_distance, get_dna_complement
+
+
+while True:
+        print("1-Hamming Distance")
+        print("2-DNA Complement")
+        print("3-Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == '1':
+            dna1 = str(input("Enter dna1: "))
+            dna2 = str(input("Enter dna2: "))
+            distance = get_hamming_distance(dna1, dna2)
+            print("Hamming Distance:", distance)
+
+        elif choice == '2':
+            dna = str(input("Enter dna string: "))
+            complement = get_dna_complement(dna)
+            print("DNA Complement:", complement)
+
+        elif choice == '3':
+            print("Exiting program...")
+            
+
+        else:
+            print("Invalid choice. Please enter a valid option.")
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,22 @@
 #
+
+
+
+def get_hamming_distance(dna1, dna2):
+    value = 7
+    return  value
+    
+    distance = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            distance += 1
+    return distance
+
+    
+    def get_dna_complement(dna):
+        complement_str = {'A','T','C','G'}
+        
+    complement = ''
+    for i in range(len(dna)):  
+        complement += complement
+        return complement
