4b76a16ac56b963a363e6950e4e1ab54470fcd48
hamdirachid5379 <157082615+hamdirachid5379@users.noreply.github.com>
2024-03-30 04:14:06 +0000

    examples2

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,9 +1,3 @@
 def test_config():
     return True
 
-def create_dictionary():
-    prog_langs= {'C++:1979', 'Java:1992', 'Python:1996', 'C#:2001'}
-    
-    #value = prog_langs.values(["python"])
-    print(prog_langs)
-    print(prog_langs.get(['C++']))
--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -6,8 +6,5 @@ class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
-'''
-    def test_dictionary_key_access(self):
-        prog_langs= {'C++:1979', 'Java:1992', 'Python:1996', 'C#:2001'}
-        self.assertEqual(prog_langs(['C++'], '1979'))
-'''
+
+    
